c1988da2a5f60a771e9419ba5111448b085ee5a1
Nathan Hugon <nthugon@gmail.com>
2016-10-27 15:32:18 -0700

    GET request works and test passes

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -10,6 +10,18 @@ describe('My HTTP Server', () => {
 
   let request = chai.request(server.httpServer);
 
+  it('lists all the boardsports', done => {
+    request
+      .get('/boardsports')
+      .end((err, res) => {
+        if (err) return done(err);
+        var boardSports = 'snowskating.json<br>surfing.json<br>';
+        assert.ok(!err);
+        assert.deepEqual(res.text, boardSports);
+        done();     
+      });
+  });
+
 
   it('posts POST on page', done => {
     postObject = {name: "skateboarding", environment: "city", weather: "clear", equipment: "skateboard, shoes"};
@@ -23,12 +35,12 @@ describe('My HTTP Server', () => {
         sander.readFile(server.filePath)
           .then(function(data){
             var newSport = JSON.parse(data);
-            console.log('newly saved object:', newSport);
             assert.deepEqual(newSport, postObject);
           })
+          .then(sander.unlink(server.filePath))
           .then(done);
 
-          
+
         // fs.readFile(server.filePath, 'utf-8', (err, data) => {
         //   if (err) throw err;
         //   var newSport = JSON.parse(data);
